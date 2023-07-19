59cafaabf3233de4e280ca58fc2871ea178f075c
Matheus <matheusbonfim0012@gmail.com>
2023-07-19 08:10:27 -0300

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch main # Your branch is up to date with 'origin/main'. # # Changes to be committed: #    modified:   index.html #    modified:   js/main.js #

--- js/main.js
+++
@@ -1,7 +0,0 @@
-Q.push(function() {
-	var input = document.getElementById("chosen_date"),
-		form = document.getElementById("bookings_date");
-	input.addEventListener("change", function(event) {
-		form.submit();
-	});
-});
